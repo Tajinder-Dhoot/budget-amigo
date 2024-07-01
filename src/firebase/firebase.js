b1548134e748c302f7dc0066b9a3ee52497aa779
Tajinder Singh Dhoot <tajindersinghdhoot@singh-on-air.home>
2024-07-01 12:41:19 -0400

    force configure projectId in firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
-import firebaseConfig from './firebaseConfig'
+import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from './firebaseConfig';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+const app = initializeApp({projectId: firebaseConfig?.projectId, ...firebaseConfig});
+
 const db = getFirestore(app);
 
 export default db;
